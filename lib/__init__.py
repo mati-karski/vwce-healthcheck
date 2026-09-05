496dc88a55485601414740381eaf1fbc3668445b
Mati <215580947+mati-karski@users.noreply.github.com>
2026-09-06 00:05:52 +0200

    Fix imports: use lib instead of tickerhealthcheck

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
-from tickerhealthcheck.data_fetcher import DataFetcher, DataFetchError
-from tickerhealthcheck.metrics import MetricsCalculator, PerformanceMetrics
-from tickerhealthcheck.health_score import (
+from lib.data_fetcher import DataFetcher, DataFetchError
+from lib.metrics import MetricsCalculator, PerformanceMetrics
+from lib.health_score import (
     HealthScoreComponent,
     HealthScoreResult,
     calculate_health_score,
